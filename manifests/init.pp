ebb674ccfe97054d2c7507be5a40ba86df8ebabd
Felipe F Quintella <felipe@quintella.com>
2018-01-24 15:38:43 -0200

    INC

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,12 +10,15 @@ class firewallmanager (
   $enabled = false,
   $purge = false,
   $allow_icmp = true,
+  $allow_sshd = true,
   $block_default = true,
   $allow_localhost = true,
   $ports = {}
   ) {
 
     include stdlib
+
+    notify { 'Appling firewall rules':}
 
     # Only supported os so far
     if $::os['family'] == 'RedHat' {
@@ -32,6 +35,7 @@ class firewallmanager (
         class { 'firewallmanager::pre':
           allow_icmp      => $allow_icmp,
           allow_localhost => $allow_localhost,
+          allow_sshd      => $allow_sshd,
         }
 
         class { 'firewallmanager::post':
@@ -44,12 +48,13 @@ class firewallmanager (
 
         class { 'firewall': }
 
-        $tmpports = lookup('frwRule::ports', Hash, 'hash', {})
-        $tmpports2 = lookup('iptables::ports', Hash, 'hash', {})
+        $tmpports = lookup('frwRule::ports', Hash, 'deep', {})
+        $tmpports2 = lookup('iptables::ports', Hash, 'deep', {})
 
         $tmpports_def = deep_merge($tmpports, $tmpports2)
 
-        $real_ports = deep_merge($ports, $tmpports)
+        $real_ports = deep_merge($ports, $tmpports_def)
+
 
         #notify {"DEBUG real_ports value":
         #  message => $real_ports,
@@ -58,28 +63,63 @@ class firewallmanager (
         $i = 100
 
         $real_ports.each | String $cmd_port, Hash $proto_cmd| {
+          $i = $i + 1
           $proto_cmd.each | String $protocol, String $command| {
 
-            $i = $i + 1
-
             if $command == 'allow' {
-              notify {"Opening port:${cmd_port} proto:${protocol}": }
+              #notify {"Opening port:${cmd_port} proto:${protocol}": }
               firewall { "${i} ${command} inbound port ${cmd_port} ${protocol}":
-                dport    => $cmd_port,
-                proto    => $protocol,
-                action   => accept,
+                dport  => $cmd_port,
+                proto  => $protocol,
+                action => accept,
               }
             }
             if $command == 'drop' {
-              notify {"Dropping port:${cmd_port} proto:${protocol}": }
+              #notify {"Dropping port:${cmd_port} proto:${protocol}": }
               firewall { "${i} ${command} inbound port ${cmd_port} ${protocol}":
-                dport    => $cmd_port,
-                proto    => $protocol,
-                action   => drop,
+                dport  => $cmd_port,
+                proto  => $protocol,
+                action => drop,
               }
             }
           }
         }
+
+        $tmprules = lookup('frwRule::rules', Array[Hash], 'deep', [])
+
+        $real_rules = $tmprules
+
+        $rule_port = undef
+        $rule_protocol = undef
+        $rule_action = undef
+        $rule_source = undef
+        $rule_sourcemask = undef
+        $rule_destination = undef
+        $rule_destinationmask = undef
+        $rule_chain = undef
+        $rule_table = undef
+
+
+        $z = 200
+        $real_rules.each | Hash $rule| {
+          $z = $z + 1
+
+          #notify { "New rule ${rule} ": }
+
+          firewall { "${z} ${rule['action']} table ${rule['table']} chain ${rule['chain']} port ${rule['port']} ${rule['protocol']} source ${rule['source']}/${rule['sourcemask']} dest ${rule['destination']}/${rule['destinationmask']}":
+            dport       => $rule['port'],
+            proto       => $rule['protocol'],
+            action      => $rule['action'],
+            chain       => $rule['chain'],
+            table       => $rule['table'],
+            source      => "${rule['source']}/${rule['sourcemask']}",
+            destination => "${rule['destination']}/${rule['destinationmask']}",
+          }
+
+
+        }
+
+
 
         #$frwRule::allow_icmp: = lookup('frwRule', Hash, 'hash')
       }
